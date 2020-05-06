5e7b56ef1f208d6b65743f765e2d6f4b6ed7fa80
ivanovant <you@example.com>
2020-05-06 22:17:06 +0300

    feat: add gulp-autoprefixer in gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ const browserSync = require('browser-sync').create();
 // const cleanCSS = require('gulp-clean-css');
 // const rename = require('gulp-rename');
 const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
 
 
 // Static server
@@ -33,6 +34,9 @@ function bs() {
 function serveSass() {
   return src('./sass/*.sass')
     .pipe(sass())
+    .pipe(autoprefixer({
+      cascade: false
+  }))
     .pipe(dest('./css'))
     .pipe(browserSync.stream());
 };
