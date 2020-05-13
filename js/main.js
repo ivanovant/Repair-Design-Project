25826c0d5a3f7697cc121e3309ef069a5c666acf
ivanovant <you@example.com>
2020-05-13 21:15:19 +0300

    feat: add js functions for modal

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function(event) { 
   const modal = document.querySelector('.modal');
-  const modalBtn = document.querySelectorAll('[data-toggle=modal]')
+  const modalBtn = document.querySelectorAll('[data-toggle=modal]');
   const closeBtn = document.querySelector('.modal__close');
   const switchModal = () => {
     modal.classList.toggle('modal--visible');
@@ -11,4 +11,14 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
   closeBtn.addEventListener('click', switchModal);
 
+  document.addEventListener('keydown', (event) => {
+    if (event.key === "Escape") {
+        switchModal();
+    }
+  });
+  modal.addEventListener('click', (event) => {
+    if (event.target === modal) {
+      switchModal();
+    }
+  });
 });
