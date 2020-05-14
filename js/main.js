f9263b0f7f5731b5b840dd1ee805a896e9683b29
ivanovant <you@example.com>
2020-05-14 18:47:59 +0300

    feat: add jquery-3.5.0.min.js in js & rewrite main.js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+/*
 document.addEventListener("DOMContentLoaded", function(event) { 
   const modal = document.querySelector('.modal');
   const modalBtn = document.querySelectorAll('[data-toggle=modal]');
@@ -19,5 +20,25 @@ document.addEventListener("DOMContentLoaded", function(event) {
     if (event.target === modal) {
       switchModal();
     }
+  });
+});
+*/
+$(document).ready(function () {
+  const modal = $('.modal'),
+        modalBtn = $('[data-toggle=modal]'),
+        closeBtn = $('.modal__close');
+  modalBtn.on('click', function () {
+    modal.toggleClass('modal--visible');
+  });
+  closeBtn.on('click', function () {
+    modal.toggleClass('modal--visible')
+  });
+  $(document).on('keydown', function (e) {
+    if ((e.key === 'Escape') && (modal.hasClass('modal--visible')))
+       modal.toggleClass('modal--visible')
+  });
+  $(document).on('click', function (e) {
+    if (modal.is(e.target))
+      modal.toggleClass('modal--visible');
   });
 });
