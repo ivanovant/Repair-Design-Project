187dbb7acb40dc8d09d1f61c5e98e5873c1d747d
ivanovant <you@example.com>
2020-05-14 15:59:42 +0300

    feat: add contains method in main.js

--- js/main.js
+++ js/main.js
@@ -2,9 +2,8 @@ document.addEventListener("DOMContentLoaded", function(event) {
   const modal = document.querySelector('.modal');
   const modalBtn = document.querySelectorAll('[data-toggle=modal]');
   const closeBtn = document.querySelector('.modal__close');
-  const switchModal = () => {
-    modal.classList.toggle('modal--visible');
-  }
+  const switchModal = () => modal.classList.toggle('modal--visible');
+  
   modalBtn.forEach(element => {
     element.addEventListener('click', switchModal);
   });
@@ -12,7 +11,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
   closeBtn.addEventListener('click', switchModal);
 
   document.addEventListener('keydown', (event) => {
-    if (event.key === "Escape") {
+    if (event.key === "Escape" && modal.classList.contains('modal--visible')) {
         switchModal();
     }
   });
