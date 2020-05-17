8a5c01ac437a37fb04b77e130fc1b94321ea8da8
ivanovant <you@example.com>
2020-05-17 21:13:43 +0300

    feat: add steps section in idex.html & add styles for steps section

--- js/main.js
+++ js/main.js
@@ -56,24 +56,42 @@ $(document).ready(function () {
       return false;
   });
 
-  var mySwiper = new Swiper ('.swiper-container', {
+  var swiper1 = new Swiper ('.swiper1', {
     loop: true,
     pagination: {
-      el: '.swiper-pagination',
+      el: '.swiper-pagination1',
       type: 'bullets',
     },
     navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
+      nextEl: '.swiper-button-next1',
+      prevEl: '.swiper-button-prev1',
     },
   })
 
-  let next = $('.swiper-button-next');
-  let prev = $('.swiper-button-prev');
-  let bullets = $('.swiper-pagination');
+  let next = $('.swiper-button-next1');
+  let prev = $('.swiper-button-prev1');
+  let bullets = $('.swiper-pagination1');
 
   next.css('left', prev.width() + 20 + bullets.width() + 20)
   bullets.css('left', prev.width() + 20)
 
+  var swiper2 = new Swiper ('.swiper2', {
+    loop: true,
+    pagination: {
+      el: '.swiper-pagination2',
+      type: 'bullets',
+    },
+    navigation: {
+      nextEl: '.swiper-button-next2',
+      prevEl: '.swiper-button-prev2',
+    },
+  })
+
+  let next2 = $('.swiper-button-next2');
+  let prev2 = $('.swiper-button-prev2');
+  let bullets2 = $('.swiper-pagination2');
+
+  next2.css('left', prev2.width() + 20 + bullets2.width() + 20)
+  bullets2.css('left', prev2.width() + 20)
 });
 
