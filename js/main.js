83fe4d9e3d90f76ca0d2daabc70a5efae2da85d2
ivanovant <you@example.com>
2020-05-20 23:11:39 +0300

    feat: add footer map  & yandex map API

--- js/main.js
+++ js/main.js
@@ -226,4 +226,37 @@ $(document).ready(function () {
   });
 
   $('[type=tel]').mask('+7 (000) 000 00-00', {placeholder: "+7 (___) ___-__-__"});
+
+  ymaps.ready(function () {
+    var myMap = new ymaps.Map('map', {
+            center: [47.208901, 39.631539],
+            zoom: 15
+        }, {
+            searchControlProvider: 'yandex#search'
+        }),
+
+        // Создаём макет содержимого.
+        MyIconContentLayout = ymaps.templateLayoutFactory.createClass(
+            '<div style="color: #FFFFFF; font-weight: bold;">$[properties.iconContent]</div>'
+        ),
+
+        myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
+            hintContent: 'Наш офис',
+            balloonContent: 'Вход со двора'
+        }, {
+            // Опции.
+            // Необходимо указать данный тип макета.
+            iconLayout: 'default#image',
+            // Своё изображение иконки метки.
+            iconImageHref: 'img/location.png',
+            // Размеры метки.
+            iconImageSize: [32, 32],
+            // Смещение левого верхнего угла иконки относительно
+            // её "ножки" (точки привязки).
+            iconImageOffset: [-5, -38]
+        })
+
+    myMap.geoObjects
+        .add(myPlacemark)
+  });
 });
