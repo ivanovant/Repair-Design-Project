d4eb21ef2624589d1e627f70aba2eeb7a97b982f
ivanovant <you@example.com>
2020-05-23 23:07:29 +0300

    refact: optimizing

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,16 @@
 const {src, dest, watch} = require('gulp');
 const browserSync = require('browser-sync').create();
-// const cleanCSS = require('gulp-clean-css');
+const cleanCSS = require('gulp-clean-css');
 // const rename = require('gulp-rename');
 const sass = require('gulp-sass');
 const autoprefixer = require('gulp-autoprefixer');
 const minImg = require('gulp-tinypng-compress');
-
+const webp = require('gulp-webp');
+const concatCSS = require('gulp-concat-css');
 
 // Static server
 function bs() {
   serveSass();
-  // mincss();
   browserSync.init({
     proxy: "Repair-Design-Project",
   });
@@ -24,40 +24,44 @@ function bs() {
   // watch('./css/*.css', mincss);
 };
 
-// Minify css files
-// function mincss() {
-//   return src("./css/*.css")
-//     .pipe(cleanCSS())
-//     .pipe(rename({ suffix: '.min'}))
-//     .pipe(dest("./css/css-min"))
-//     .pipe(browserSync.stream());
-// };
 
 // Compressing jpg&png images
 function tinypng() {
-  return src("./img/map-load-bg.jpg")
+  return src("./img/clients/*.jpg")
     .pipe(minImg({
       key: 'yY89MnhWVL6TNTN5l7b4lQPtcxzw3f17',
       sigFile: 'images/.tinypng-sigs',
       log: true
     }))
     .pipe(dest("./img/minimg"));
-};
-
-
-// compilate sass files
-function serveSass() {
-  return src("./sass/**/*.sass", "./sass/**/*.scss")
+  };
+  
+  // compilate sass files
+  function serveSass() {
+    return src("./sass/**/*.sass", "./sass/**/*.scss")
     .pipe(sass())
     .pipe(autoprefixer({
       cascade: false
-  }))
+    }))
     .pipe(dest('./css'))
     .pipe(browserSync.stream());
-};
+  };
+  
+  function imgToWebp() {
+    return src("./img/design/*.png")
+    .pipe(webp())
+    .pipe(dest("./img/webp"));
+  };
 
-
-
-
-exports.serve = bs;
-exports.minimg = tinypng;
+  // Minify css files
+  function mincss() {
+    return src("./css/*.css")
+      .pipe(cleanCSS())
+      .pipe(concatCSS("main.css"))
+      .pipe(dest("./css/min"))
+  };
+  
+  exports.serve = bs;
+  exports.minimg = tinypng;
+  exports.webp = imgToWebp;
+  exports.css = mincss;
--- js/main.js
+++ js/main.js
@@ -280,19 +280,10 @@ $(document).ready(function () {
 
   $('[type=tel]').mask('+7 (000) 000 00-00', {placeholder: "+7 (___) ___-__-__"});
 
-
-  VK.Widgets.Subscribe("vk__modal", {
-    width: 320,
-    soft: 1,
-    },
-  -123083697);
-
-  //Переменная для включения/отключения индикатора загрузки
   let spinner = $('.ymap-container').children('.loader');
-  //Переменная для определения была ли хоть раз загружена Яндекс.Карта (чтобы избежать повторной загрузки при наведении)
+
   let check_if_load = false;
-  
-  //Функция создания карты сайта и затем вставки ее в блок с идентификатором &#34;map-yandex&#34;
+
   function init () {
     let myMap = new ymaps.Map('map-yandex', {
         center: [47.208901, 39.631539],
@@ -308,23 +299,18 @@ $(document).ready(function () {
         balloonContent: 'Вход со двора'
     }, {
         iconLayout: 'default#image',
-        iconImageHref: '../img/minimg/img/location.png',
+        iconImageHref: '../img/location.png',
         iconImageSize: [32, 32],
         iconImageOffset: [-5, -38],
     });
-  
     myMap.geoObjects.add(myPlacemark);
-    // Получаем первый экземпляр коллекции слоев, потом первый слой коллекции
     let layer = myMapTemp.layers.get(0).get(0);
-  
-    // Решение по callback-у для определения полной загрузки карты
+
     waitForTilesLoad(layer).then(function() {
-      // Скрываем индикатор загрузки после полной загрузки карты
       spinner.removeClass('is-active');
     });
   }
   
-  // Функция для определения полной загрузки карты (на самом деле проверяется загрузка тайлов) 
   function waitForTilesLoad(layer) {
     return new ymaps.vow.Promise(function (resolve, reject) {
       let tc = getTileContainer(layer), readyAll = true;
@@ -357,41 +343,21 @@ $(document).ready(function () {
     return null;
   }
   
-  // Функция загрузки API Яндекс.Карт по требованию (в нашем случае при наведении)
   function loadScript(url, callback) {
     let script = document.createElement("script");
-  
-    if (script.readyState){  // IE
-      script.onreadystatechange = function() {
-        if (script.readyState == "loaded" ||
-                script.readyState == "complete") {
-          script.onreadystatechange = null;
-          callback();
-        }
-      };
-    } else {  // Другие браузеры
       script.onload = function() {
         callback();
       };
-    }
     script.src = url;
     document.getElementsByTagName("head")[0].appendChild(script);
   }
-  
-  // Основная функция, которая проверяет когда мы навели на блок с классом &#34;ymap-container&#34;
+
   let ymap = function() {
     $('.ymap-container').mouseenter(function() {
-        if (!check_if_load) { // проверяем первый ли раз загружается Яндекс.Карта, если да, то загружаем
-  
-        // Чтобы не было повторной загрузки карты, мы изменяем значение переменной
+        if (!check_if_load) { 
           check_if_load = true; 
-  
-      // Показываем индикатор загрузки до тех пор, пока карта не загрузится
           spinner.addClass('is-active');
-  
-      // Загружаем API Яндекс.Карт
           loadScript("https://api-maps.yandex.ru/2.1/?apikey=a00f0ba8-875e-472d-9d6e-105c41b610b1&lang=ru_RU", function() {
-            // Как только API Яндекс.Карт загрузились, сразу формируем карту и помещаем в блок с идентификатором &#34;map-yandex&#34;
             ymaps.load(init);
           });                
         }
