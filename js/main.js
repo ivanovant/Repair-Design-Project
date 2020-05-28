97cda48200cc33505bce88dd9bf2c1d4ae4d85db
ivanovant <you@example.com>
2020-05-28 18:37:43 +0300

    refact: rewrite main.js

--- js/main.js
+++ js/main.js
@@ -494,4 +494,10 @@ $(document).ready(function () {
     event.target.playVideo();
   };
 
+  $(document).one('mouseenter', (e) => {
+    if (e.currentTarget = 'document') {
+      $( "<script type=\"text/javascript\" > (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)}; m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)}) (window, document, \"script\", \"https://cdn.jsdelivr.net/npm/yandex-metrica-watch/tag.js\", \"ym\"); ym(64410109, \"init\", { clickmap:true, trackLinks:true, accurateTrackBounce:true, webvisor:true }); </script> <noscript><div><img src=\"https://mc.yandex.ru/watch/64410109\" style=\"position:absolute; left:-9999px;\" alt=\"\" /></div></noscript>").appendTo('head')
+    }
+  });
+
 });
